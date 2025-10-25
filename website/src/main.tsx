f6303b41ed57a91903702bd718dde3553c6ae462
pvlov <Daniel.Pavlov@gmx.de>
2025-10-25 21:30:09 +0200

    use hashrouter

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -2,18 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "@/App.tsx";
 import "@/index.css";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import Blog from "@/sections/Blog.tsx";
 import Collection from "./sections/Collection.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/collection" element={<Collection />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
