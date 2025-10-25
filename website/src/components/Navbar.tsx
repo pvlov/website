ff8d9b25c54dcd9360bca696dd322d572d6cd738
pvlov <Daniel.Pavlov@gmx.de>
2025-10-25 22:13:16 +0200

    remove link to about for now

--- website/src/components/Navbar.tsx
+++ website/src/components/Navbar.tsx
@@ -25,12 +25,12 @@ const Navbar: React.FC = () => {
         ~pvlov.de
       </Link>
       <nav className="flex items-center gap-4 sm:gap-10">
-        <a
+        {/* <a
+          href="/#about"
           className="text-white hover:text-white transition-colors cursor-pointer"
-          onClick={() => document.getElementById('about')?.scrollIntoView()}
         >
           about
-        </a>
+        </a> */}
         <a
           href="/cv.pdf"
           target="_blank"
